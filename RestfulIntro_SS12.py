2b4db54f3f418adc4934f1b878675c4c5f6fff5e
hp-PC\hp <rabac1998@gmail.com>
2016-12-30 17:47:16 +0700

    commit

--- RestfulIntro_SS12.py
+++ RestfulIntro_SS12.py
@@ -37,20 +37,20 @@ mongoengine.connect(
     username = "admin",
     password = "141298"
 )
-class Music(Document): #Music
+class User(Document): #Music
     yourname = StringField()
     songname = StringField()
     artists = StringField()
     image = StringField()
     link = StringField()
 
-print(Music.objects)
+print(User.objects)
 
 class MusicListRes(Resource):
     def get(self): #read
         #return [json.loads(zodiac.to_json()) for zodiac in Zodiac.objects]
         #return [json.loads(zodiac.to_json()) for zodiac in Zodiac.objects]
-        return meList2json(Music.objects)
+        return meList2json(User.objects)
     def post(self): #Create
         args = parser.parse_args()
         yourname  = args['yourname']
@@ -60,20 +60,20 @@ class MusicListRes(Resource):
         link = args['link']
         print(yourname,songname,image,artists,link)
         return{"Status" : "OK"}
-        music = Music(yourname = yourname,songname= songname,image= image,artists= artists, link = link)
+        music = User(yourname = yourname,songname= songname,image= image,artists= artists, link = link)
         music.save()
 
 
 class MusicRes(Resource):
     def get(self,music_id):
-        return json.loads(Music.objects().with_id(music_id).to_json())
+        return json.loads(User.objects().with_id(music_id).to_json())
     def delete(self,music_id):
-        all_musics = Music.objects()
+        all_musics = User.objects()
         found_music = all_musics.with_id(music_id)
         found_music.delete()
         return{"code":"1" , "status": "OK"}
     def put(self,music_id):
-        all_musics = Music.objects()
+        all_musics = User.objects()
         found_music = all_musics.with_id(music_id)
         args = parser.parse_args()
         yourname = args['yourname']
@@ -82,14 +82,14 @@ class MusicRes(Resource):
         artists = args['artists']
         link = args['link']
         found_music.update(set__yourname=yourname,set__songname=songname,set__image=image,set__artists=artists,set__link= link )
-        return json.loads(Music.objects().with_id(music_id).to_json())
+        return json.loads(User.objects().with_id(music_id).to_json())
 api.add_resource(MusicListRes,"/api/music") #Get all, post
 api.add_resource(MusicRes,"/api/music/<music_id>") #Get one, PUT,DELETE
 def me2json(item):
     return json.loads(item.to_json())
 def meList2json(list):
     return[me2json(item) for item in list]
-print(meList2json(Music.objects))
+print(meList2json(User.objects))
 
 
 if __name__ == '__main__':
